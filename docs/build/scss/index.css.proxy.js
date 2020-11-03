8cd81b3752adea11fe3b119d081621565aa6e4d0
Alex <alexilin83@gmail.com>
2020-11-03 12:38:04 +0300

    fixes

--- docs/build/scss/index.css.proxy.js
+++ docs/build/scss/index.css.proxy.js
@@ -1,6 +1,6 @@
 // [snowpack] add styles to the page (skip if no document exists)
 if (typeof document !== 'undefined') {
-  const code = "body {\n  text-align: center;\n}\n\n.hidden {\n  display: none;\n}\n\n.container {\n  max-width: 1200px;\n  margin: auto;\n}\n\n.users {\n  display: flex;\n  justify-content: center;\n}\n.users__item {\n  width: 100%;\n  max-width: 500px;\n  margin: 0 10px;\n}\n.users__item video {\n  --width: 100%;\n  width: var(--width);\n  height: calc(var(--width) * 0.75);\n  height: auto;\n  margin: 0 0 20px;\n  background: grey;\n}\n\n.form-group {\n  margin: 0 0 20px;\n}";
+  const code = "body {\n  text-align: center;\n}\n\n.hidden {\n  display: none;\n}\n\n.container {\n  max-width: 1200px;\n  margin: auto;\n}\n\n.users {\n  display: flex;\n  justify-content: center;\n}\n.users__item {\n  width: 300px;\n  margin: 0 10px;\n}\n.users__item video {\n  width: 100%;\n  height: auto;\n  margin: 0 0 20px;\n  background: grey;\n}\n\n.form-group {\n  margin: 0 0 20px;\n}";
 
   const styleEl = document.createElement("style");
   const codeEl = document.createTextNode(code);
